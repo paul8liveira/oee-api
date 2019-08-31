b247c545ecee6e2a7e0db875b7d3e77650e52361
Paulo Oliveira <paulooliveira@pecege.com>
2019-08-31 14:32:31 -0300

    estrutura de responsaveis e alertas de pausa

---
+++ api/models/alert.js
@@ -0,0 +1,86 @@
+module.exports = function(api) {
+    let _pool = api.database.connection; 
+
+    this.save = function(data, callback) {
+        let query = 'call prc_alert(?,?,?,?);';
+        
+        _pool.getConnection(function(err, connection) {
+            connection.query(query, 
+            [
+                data.channel_id,
+                data.sponsor_id,
+                data.pause_reason_id,
+                data.pause_time,
+            ], 
+            function(error, result) {
+                connection.release();
+                callback(error, result);
+            });
+        });
+    };
+    
+    this.update = function(data, callback) {
+        let query = `update alert 
+                        set sponsor_id = ?, 
+                            pause_reason_id = ?,
+                            pause_time = ?
+                      where id = ?`;
+        
+        _pool.getConnection(function(err, connection) {
+            connection.query(query, 
+            [
+                data.sponsor_id, 
+                data.pause_reason_id,
+                data.pause_time,
+                parseInt(data.id)
+            ], 
+            function(error, result) {
+                connection.release();
+                callback(error, result);
+            });
+        });
+    };
+    
+    this.delete = function(data, callback) {    
+        _pool.getConnection(function(err, connection) {
+            connection.query("call prc_delete_alert(?)", 
+            [
+                parseInt(data.id)
+            ], 
+            function(error, result) {
+                connection.release();
+                callback(error, result);
+            });
+        });    
+    };
+    
+    this.list = function(channelId, callback) {
+        var query = `
+            select a.id
+            , a.channel_id
+            , c.name as channel_name
+            , a.sponsor_id
+            , s.name as sponsor_name
+            , a.pause_reason_id
+            , pr.name as pause_reason_name
+            , a.pause_time
+        from alert a
+        inner join channel c on c.id = a.channel_id
+        inner join sponsor s on s.id = a.sponsor_id
+        inner join pause_reason pr on pr.id = a.pause_reason_id
+        where channel_id = ?`;
+        
+        _pool.getConnection(function(err, connection) {
+            connection.query(query, 
+            [ 
+                parseInt(channelId)
+            ], 
+            function(error, result) {
+                connection.release();
+                callback(error, result);
+            });
+        });
+    };
+
+    return this;
+};
