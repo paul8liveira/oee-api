c875a0020c243f6d3121a24d6c8bbfa88dfd0fef
Paulo Oliveira <paulooliveira@pecege.com>
2019-09-09 22:06:50 -0300

    ajustes no lançamento de pausa com alerta

--- api/controllers/machinePauseDash.js
+++ api/controllers/machinePauseDash.js
@@ -3,12 +3,12 @@ module.exports = function(api) {
     const _alert = api.models.alert;
     const _mailer = api.services.mailer;
     
-    this.post = function(req, res, next) {
-        let bodyData = req.body;
+    this.hasAlert = function(req, res, next) {
+        const bodyData = req.body;
 
-        let pauseFilter = {
-            channel_id: bodyData[0].channel_id,
-            machine_code: bodyData[0].machine_code,            
+        const pauseFilter = {
+            channel_id: bodyData[0].channel_id, 
+            machine_code: bodyData[0].machine_code,  
             date_ini: bodyData[0].date_ref,
             date_fin: bodyData[1].date_ref,
             pause_reason_id: bodyData[0].pause_reason_id,
@@ -17,11 +17,11 @@ module.exports = function(api) {
 
         //verifica se tem alerta de pausa para enviar email
         _alert.hasAlertToSend(pauseFilter, async function(exception, alerts) {
-            //se houve erro aqui, segue o processo e s´´o faz um console
+            //se houve erro aqui, segue o processo e só faz um console
             if(exception) console.error(exception);
             
-            //envia email (sem formatação e detalhes por enquanto)
-            if(alerts) {
+            //envia email (sem formatação e detalhes por enquanto)    
+            if(alerts && alerts.length > 0) {
                 const pauseReasonName = alerts[0].pause_reason_name;
                 const mailsToAlert = alerts.map(m => m.sponsor_email).join(',');
                 const html = `
@@ -31,18 +31,30 @@ module.exports = function(api) {
                     <br>Data final da pausa: ${pauseFilter.date_fin}
                 `;
                 await _mailer.send(mailsToAlert, 'Alerta de pausa', html);
-            }
-            
-            _machinePauseDash.save(pauseFilter, function(exception, result) {
-                if(exception) {
-                    return res.status(400).send(exception);
-                }     
-                return res.status(200).send(result);           
-            });             
+            }            
+            next();
+        });                                         
+    };
+    
+    this.post = function (req, res, next) {
+        const bodyData = req.body;
 
-        });         
-                        
-    }; 
+        const pauseFilter = {
+            channel_id: bodyData[0].channel_id,
+            machine_code: bodyData[0].machine_code,            
+            date_ini: bodyData[0].date_ref,
+            date_fin: bodyData[1].date_ref,
+            pause_reason_id: bodyData[0].pause_reason_id,
+            pause: bodyData[1].date_dif   
+        };
+
+        _machinePauseDash.save(pauseFilter, function(exception, result) {
+            if(exception) {
+                return res.status(400).send(exception);
+            }     
+            return res.status(200).send(result);           
+        });  
+    };
 
     return this;
 };
