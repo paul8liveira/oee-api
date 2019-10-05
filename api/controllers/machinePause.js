5d26cff80708dc9672a2608c6bd2092469d2efb4
Paulo Oliveira <paulooliveira@pecege.com>
2019-10-05 10:53:59 -0300

    adicionando alerta de email

--- api/controllers/machinePause.js
+++ api/controllers/machinePause.js
@@ -1,5 +1,40 @@
 module.exports = function(api) {
     let _machinePause = api.models.machinePause;
+    const _alert = api.models.alert;
+    const _mailer = api.services.mailer;
+    
+    this.hasAlert = function(req, res, next) {
+        var query = req.query;
+
+        const pauseFilter = {
+            channel_id: 14, //por enquanto vou deixar fixo, só pra teste
+            machine_code: query.mc,  
+            date_ini: query.sd,
+            date_fin: query.ed,
+            pause_reason_id: query.pr,
+            pause: query.p   
+        };
+
+        //verifica se tem alerta de pausa para enviar email
+        _alert.hasAlertToSend(pauseFilter, async function(exception, alerts) {
+            //se houve erro aqui, segue o processo e só faz um console
+            if(exception) console.error(exception);
+            
+            //envia email (sem formatação e detalhes por enquanto)    
+            if(alerts && alerts.length > 0) {
+                const pauseReasonName = alerts[0].pause_reason_name;
+                const mailsToAlert = alerts.map(m => m.sponsor_email).join(',');
+                const html = `
+                    A pausa <b>${pauseReasonName}</b> de <b>${pauseFilter.pause} minutos</b> foi lançada.
+                    <br>Máquina: ${pauseFilter.machine_code}
+                    <br>Data inicial da pausa: ${pauseFilter.date_ini}
+                    <br>Data final da pausa: ${pauseFilter.date_fin}
+                `;
+                await _mailer.send(mailsToAlert, 'Alerta de pausa', html);
+            }            
+            next();
+        });                                         
+    };    
 
     this.save = function(req, res, next) {
         var query = req.query;  
--- api/routes/machinePause.js
+++ api/routes/machinePause.js
@@ -4,7 +4,7 @@ module.exports = function(api) {
     const _machinePauseController = api.controllers.machinePause;
     const _tokenController = api.controllers.token;
 
-    router.get('/', _machinePauseController.save); //iot
+    router.get('/', _machinePauseController.hasAlert, _machinePauseController.save); //iot
     router.get('/list', _tokenController.verify, _machinePauseController.list);
     router.get('/pareto', _tokenController.verify, _machinePauseController.pareto);
 
