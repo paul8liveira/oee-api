f6355b499259a57a181a8dbeb99217f30a2177bd
Paulo Oliveira <paulooliveira@pecege.com>
2021-01-26 20:21:13 -0300

    event_daily_oee

---
+++ mysql/14-17012021_23012021/event_daily_oee.sql
@@ -0,0 +1,10 @@
+create event if not exists event_daily_oee
+	on schedule
+		every 1 day
+		starts '2021-01-26 00:00:00'
+		comment 'Inserir dados na tabela daily_oee'
+    do
+		call prc_daily_oee(14, date_format(now(), '%Y-%m-%d'));
+        
+        
+        
